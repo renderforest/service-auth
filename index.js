a1fd2e4b6a41ced2df2d319e6401e17063ebd219
Albert Hambardzumyan <hambardzumyan.albert@gmail.com>
2017-10-17 12:15:34 +0400

    service: add index.js file to re-export lib function.

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const {authorize} = require('./lib/authorize')
+const {setAuthorization} = require('./lib/set-authorization')
+
+module.exports = {
+  authorize,
+  setAuthorization
+}
