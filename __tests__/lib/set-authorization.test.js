5df4329924709cc481156728f7e16951685474ac
narekhovhannisyan <narekgilmour@gmail.com>
2017-10-23 17:26:02 +0400

    test: normalized set-authorization function test in __test__/lib
     - removed __mock__ from root

--- __tests__/lib/set-authorization.test.js
+++ __tests__/lib/set-authorization.test.js
@@ -1,21 +1,20 @@
-const setAuth = require('../../__mock__/lib/set-authorization.mock')
+const setAuthorization = require('../../src/lib/set-authorization')
 
 test('setAuthorization function test', () => {
   const options = {
-    'foo': 'baar',
-    'fuzz': 'foo'
+    headers: {
+      authorization: 'mock-auth',
+      nonce: 'mock-nonce'
+    },
+    uri: 'https://mock-mock.com',
+    body: 'mock-body'
   }
-  const signKey = 'key'
-  const clientid = 10
+  const signKey = 'mock-signKey'
+  const clientid = 'mock'
 
-  expect(setAuth.setAuthorization(options, signKey, clientid)).toEqual(
-    { 'foo': 'baar',
-      'fuzz': 'foo',
-      "headers": {
-        "authorization": signKey,
-        "clientid": clientid,
-        "nonce": "nonce",
-        "timestamp": "date" }
-    }
-  )
+  expect(typeof setAuthorization.setAuthorization(options, signKey, clientid)).toBe('object')
+  expect(setAuthorization.setAuthorization(options, signKey, clientid).headers.nonce).toBeDefined()
+  expect(setAuthorization.setAuthorization(options, signKey, clientid).headers.clientid).toBe('mock')
+  expect(setAuthorization.setAuthorization(options, signKey, clientid).headers.timestamp).toBeDefined()
+  expect(setAuthorization.setAuthorization(options, signKey, clientid).headers.authorization).toBeDefined()
 })
