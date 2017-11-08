31ee1b159852d36cc7fe444e94e36f89f1a68a0d
Albert Hambardzumyan <hambardzumyan.albert@gmail.com>
2017-11-08 10:38:33 +0400

    test: add test in set-authorization checks for other headers not to be  overwritten.

--- __tests__/lib/set-authorization.test.js
+++ __tests__/lib/set-authorization.test.js
@@ -6,9 +6,25 @@ describe('set authorization: ', () => {
   describe('setAuthorization(): ', () => {
     test('should be valid (ordinary case).', () => {
       const options = {
+        headers: {},
+        uri: 'https://mock-mock.com',
+        body: 'mock-body'
+      }
+      const signKey = 'mock-signKey'
+      const clientId = 'mock-clientid'
+      const setAuth = setAuthorization.setAuthorization(options, signKey, clientId)
+
+      expect(typeof setAuth).toBe('object')
+      expect(setAuth.headers.nonce).toBeDefined()
+      expect(setAuth.headers.clientid).toBe(clientId)
+      expect(setAuth.headers.timestamp).toBeDefined()
+      expect(setAuth.headers.authorization).toBeDefined()
+    })
+
+    test('should be valid. Checks for other headers not to be overwritten.', () => {
+      const options = {
         headers: {
-          authorization: 'mock-auth',
-          nonce: 'mock-nonce'
+          other: 'mock-other'
         },
         uri: 'https://mock-mock.com',
         body: 'mock-body'
@@ -22,6 +38,7 @@ describe('set authorization: ', () => {
       expect(setAuth.headers.clientid).toBe(clientId)
       expect(setAuth.headers.timestamp).toBeDefined()
       expect(setAuth.headers.authorization).toBeDefined()
+      expect(setAuth.headers.other).toBe(options.headers.other)
     })
 
     test('should be valid. In case of `headers` is missing from `options` object.', () => {
